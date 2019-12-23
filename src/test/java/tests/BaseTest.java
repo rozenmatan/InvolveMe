9002e820cf5c82c157c15291bfe081f42f5d5bc3
Matan Rozen <rozenmatan1989@gmail.comgit config --global user.email rozenmatan1989@gmail.com>
2019-12-23 18:33:33 +0200

    add webdriver manager

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -16,7 +16,9 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import pageobjects.LoginPage;
 import pageobjects.deleteAllFromsPage;
 
@@ -30,21 +32,23 @@ public abstract class BaseTest {
 	@Parameters({ "browser" })
 	@BeforeClass
 	public void setup(ITestContext testContext, String browser) {// initiate the driver and set the URL
+		System.out.println(browser);
 		switch (browser) {
 		case "chrome":
-			WebDriverManager.chromedriver().setup();
+			//WebDriverManager.chromedriver().setup();
 			driver = new ChromeDriver();
 			break;
 		case "firefox":
-			WebDriverManager.firefoxdriver().setup();
+			//WebDriverManager.firefoxdriver().setup();
 			driver = new FirefoxDriver();
 			break;
 		case "opera":
-			WebDriverManager.operadriver().setup();
+			//WebDriverManager.operadriver().setup();
 			driver = new OperaDriver();
 			break;
 		default:
 			break;
+		}
 	}
 	
 	@BeforeClass
